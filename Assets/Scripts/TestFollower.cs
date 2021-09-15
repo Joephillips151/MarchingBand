d7a7a0ec8bc3143a73eb76e26e6bdabc782945c8
Joephillips151 <63381643+Joephillips151@users.noreply.github.com>
2021-09-15 08:01:29 -0500

    Getting access to anchor positions
    Changed Points to public, accessed it in my movement script and am printing the XYZ coordinates.
    
    Current problem is that ball position and anchor positions seem to be way different even though they shouldn't be.  Potentially a global XYZ vs local XYZ thing.  not sure.

--- Assets/Scripts/TestFollower.cs
+++ Assets/Scripts/TestFollower.cs
@@ -14,6 +14,25 @@ public class TestFollower : MonoBehaviour
     {
         distanceTravelled += speed * Time.deltaTime;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
-        print(pathCreator.bezierPath.NumAnchorPoints);
+        //Print the position of the Ball in the console
+        print(transform.position);
+
+        //The following FOR LOOP finds the anchors by iterating through the POINTS and selecting every 3rd one.  It then 
+        //prints the Anchor XYZ transform.
+        for(int i = 0; i < pathCreator.bezierPath.points.Count; i++)
+        {
+            if (i % 3 == 0)
+            print($"THE ANCHORS ARE: {string.Join(", ", pathCreator.bezierPath.points[i])}");
+            { if (Vector3.Distance(pathCreator.bezierPath.points[i], transform.position) < 1)
+                {
+                    //print(Vector3.Distance(pathCreator.bezierPath.points[i], transform.position));
+                    print(i);
+                    if (transform.position == pathCreator.bezierPath.points[i])
+                    {
+                        print($"THEYREEQUAL: {string.Join(", ", pathCreator.bezierPath.points[i])}");
+                    }
+                }
+            }    
+        }
     }
 }
